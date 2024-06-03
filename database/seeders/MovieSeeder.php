70294da1dc65708bc46cddffb827107b4ccfe2e7
asmaantrt <asmaaantakly@gmail.com>
2024-06-03 16:40:59 +0300

    Created DatabaseSeeder to add data

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Movie::create([
+            'movie_name'=>'A Part of You',
+            'image' => 'img/movie_images/a_part_of_you_poster.webp',  
+            'language'=>'Swedish',
+            'description'=>'A teenager struggles to pick up the pieces of her shattered world in this emotional and bittersweet coming-of-age drama about those who are left behind.',
+            'rating' => '7.0 / 10',
+            'run_time'=>'100 minutes',
+            'release_date' => date('2024-5-31')  ,
+            
+            
+        ]);
+    }
+}
