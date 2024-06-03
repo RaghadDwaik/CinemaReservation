aaa9ff7da1e9cca57434001c2944583a9baf43d0
asmaantrt <asmaaantakly@gmail.com>
2024-06-03 18:17:57 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Dashboard\MovieController;
 |
 */
 Route::get('/dashboard',[DashboardController::class,'index']);
-Route::get('/dashboard/movies/index',[MovieController::class,'index'])->name('dashboard.movies.index');;
+Route::get('/dashboard/movies/index',[MovieController::class,'index'])->name('dashboard.movies.index');
 
 Route::get('/', function () {
     return view('welcome');
