94262ce9c977bf1dea604993acdf515c21ae669f
raghad <raghad.dwaik22@gmail.com>
2024-06-04 11:53:14 +0200

    add home to index view

--- resources/views/dashboard/movies/index.blade.php
+++ resources/views/dashboard/movies/index.blade.php
@@ -8,6 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
+        <link rel="stylesheet" href="{{ asset('css/webstyles.css') }}">
+
         <!-- Place favicon.ico in the root directory -->
 
 		<!-- CSS here -->
@@ -40,133 +42,49 @@
         <button class="scroll-top scroll-to-target" data-target="html">
             <i class="fas fa-angle-up"></i>
         </button>
-        <!-- Scroll-top-end-->
-
-        <!-- header-area -->
         <header>
-            <div id="sticky-header" class="menu-area transparent-header">
-                <div class="container custom-container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="mobile-nav-toggler"><i class="fas fa-bars"></i></div>
-                            <div class="menu-wrap">
-                                <nav class="menu-nav show">
-                                    <div class="logo">
-                                        <a href="index.html">
-                                            <img src="{{asset('img/logo/logo.png')}}" alt="Logo">
-                                        </a>
-                                    </div>
-                                    <div class="navbar-wrap main-menu d-none d-lg-flex">
-                                        <ul class="navigation">
-                                            <li class="menu-item-has-children"><a href="#">Home</a>
-                                                <ul class="submenu">
-                                                    <li><a href="index.html">Home One</a></li>
-                                                    <li><a href="index-2.html">Home Two</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="active menu-item-has-children"><a href="#">Movie</a>
-                                                <ul class="submenu">
-                                                    <li class="active"><a href="movie.html">Movie</a></li>
-                                                    <li><a href="movie-details.html">Movie Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="tv-show.html">tv show</a></li>
-                                            <li><a href="pricing.html">Pricing</a></li>
-                                            <li class="menu-item-has-children"><a href="#">blog</a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.html">Our Blog</a></li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="contact.html">contacts</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="header-action d-none d-md-block">
-                                        <ul>
-                                            <li class="header-search"><a href="#" data-toggle="modal" data-target="#search-modal"><i class="fas fa-search"></i></a></li>
-                                            <li class="header-lang">
-                                                <form action="#">
-                                                    <div class="icon"><i class="flaticon-globe"></i></div>
-                                                    <select id="lang-dropdown">
-                                                        <option value="">En</option>
-                                                        <option value="">Au</option>
-                                                        <option value="">AR</option>
-                                                        <option value="">TU</option>
-                                                    </select>
-                                                </form>
-                                            </li>
-                                            <li class="header-btn"><a href="#" class="btn">Sign In</a></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-
-                            <!-- Mobile Menu  -->
-                            <div class="mobile-menu">
-                                <div class="close-btn"><i class="fas fa-times"></i></div>
-
-                                <nav class="menu-box">
-                                    <div class="nav-logo"><a href="index.html"><img src="{{asset('img/logo/logo.png')}}" alt="" title=""></a>
-                                    </div>
-                                    <div class="menu-outer">
-                                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
-                                    </div>
-                                    <div class="social-links">
-                                        <ul class="clearfix">
-                                            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-youtube"></span></a></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="menu-backdrop"></div>
-                            <!-- End Mobile Menu -->
-
-                            <!-- Modal Search -->
-                            <div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <form>
-                                            <input type="text" placeholder="Search here...">
-                                            <button><i class="fas fa-search"></i></button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Modal Search-end -->
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- header-area-end -->
-
-
-        <!-- main-area -->
+        <nav>
+            <ul class="navbar">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Coupon</a></li>
+                <li><a href="#" class="join-us">Join Us</a></li>
+                <li>
+                    <select>
+                        <option value="en">EN</option>
+                        <option value="ar">Arabic</option>
+                    </select>
+                </li>
+                <li class="icon"><a href="#"><i class="fas fa-user"></i></a></li>
+                <li class="icon"><a href="#"><i class="fas fa-search"></i></a></li>
+            </ul>
+        </nav>
+    </header>
         <main>
 
             <!-- breadcrumb-area -->
-            <section class="breadcrumb-area breadcrumb-bg" data-background="{{asset('img/bg/breadcrumb_bg.jpg')}}">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="breadcrumb-content">
-                                <h2 class="title">Our <span>Movie</span></h2>
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">Movie</li>
-                                    </ol>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
+            <section class="hero">
+            <div class="movie-info">
+                <h1>Harry Potter</h1>
+                <p>An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family
+                    and the terrible evil that haunts the magical world....
+                    <span style="color:#EEC438">See more</span>
+                </p>
+                <div class="movie-details">
+                    <span class="duration">1h 55m</span>
+                    <span class="language">ENGLISH</span>
                 </div>
-            </section>
+                <div class="tags">
+                    <span>Adventure</span>
+                    <span>Family</span>
+                    <span>Fantasy</span>
+                </div>
+                <button class="book-now">Book Now!</button>
+            </div>
+
+        </section>
             <!-- breadcrumb-area-end -->
 
             <!-- movie-area -->
