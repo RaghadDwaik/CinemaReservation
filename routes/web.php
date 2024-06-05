8401a51704156c3160e13a46037e9a7a02250c13
asmaantrt <asmaaantakly@gmail.com>
2024-06-05 18:59:27 +0300

    addtion 4 route (animation , drama , comedy , action

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,12 @@ use App\Http\Controllers\Dashboard\MovieController;
 |
 */
 
-Route::get('/dashboard/customers', [CustomerController::class, 'index'])->name('dashboard.customers');
+Route::get('/dashboard/customers',[CustomerController::class, 'index'])->name('dashboard.customers');
 Route::get('/dashboard/customers/create',[CustomerController::class, 'create'])->name('dashboard.customers.create');
-Route::post('dashboard/customers/store',[CustomerController::class, 'store'])->name('dashboard.customers.store')
-
+Route::post('dashboard/customers/store',[CustomerController::class, 'store'])->name('dashboard.customers.store');
 Route::get('/dashboard',[DashboardController::class,'index']);
 Route::get('/dashboard/movies/index',[MovieController::class,'index'])->name('dashboard.movies.index');
-
-
+Route::get('/dashboard/movies/animation',[MovieController::class,'animation'])->name('dashboard.movies.animation');
+Route::get('/dashboard/movies/comedy',[MovieController::class,'comedy'])->name('dashboard.movies.comedy');
+Route::get('/dashboard/movies/action',[MovieController::class,'action'])->name('dashboard.movies.action');
+Route::get('/dashboard/movies/drama',[MovieController::class,'drama'])->name('dashboard.movies.drama');
