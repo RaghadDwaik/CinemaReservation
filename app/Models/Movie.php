27f4b3d0b756193cc3bd69962331863d09b53744
asmaantrt <asmaaantakly@gmail.com>
2024-06-10 18:34:38 +0300

    add relation hasMany with  show_movie table

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -19,4 +19,9 @@ class Movie extends Model
         'release_date',
         'type',
     ];
+
+    //Laravel will automatically link the id column of the movie table to the movie_id column of the Show_Movie table.
+    public function Show_Movies(){
+        return $this -> hasMany(Show_Movie::class);
+    }
 }
