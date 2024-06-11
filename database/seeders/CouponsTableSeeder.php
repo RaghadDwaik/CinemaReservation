524a1e2fde553b87585d3c955a7b6f09e622dc31
raghad <raghad.dwaik22@gmail.com>
2024-06-11 13:26:33 +0200

    update coupon

--- database/seeders/CouponsTableSeeder.php
+++ database/seeders/CouponsTableSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // Import the DB facade
-use Illuminate\Support\Str; // Import Str for the timestamps
+use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Str; 
 
 class CouponsTableSeeder extends Seeder
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this -> call(MovieSeeder::class);
 
-       /* $this->call([
+        $this->call([
             CouponsTableSeeder::class,
-        ]);*/
+        ]);
     }
 }
