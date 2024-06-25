19083ae02df8e0cf6e97921cc7f6093ab48b88d2
raghad <raghad.dwaik22@gmail.com>
2024-06-25 22:37:13 +0200

    --

--- resources/views/dashboard/Layout/aboutus.blade.php
+++ resources/views/dashboard/Layout/aboutus.blade.php
@@ -9,7 +9,7 @@
         <h2 class="about-h2">Who We Are</h2>
         <p class="about-p">We are a website specialized in selling and booking cinema tickets, dedicated to providing an
             easy and convenient experience for purchasing movie tickets all around the world. We offer our customers the
-            opportunity to access the latest movies and series of all kinds, at competitive prices and high quality.</p>
+            opportunity to access the latest movies of all kinds, at competitive prices and high quality.</p>
     </div>
 
     <div class="about-container">
@@ -17,7 +17,7 @@
         <h2 class="about-h2">Why Us</h2>
         <p class="about-p">Because we are considered the best site in history to help you book cinema tickets with the
             highest quality all around the world and at the best prices. Yes, indeed, we are the only platform for
-            booking cinema tickets where you will find all types of movies and series that you have always wanted to
+            booking cinema tickets where you will find all types of movies that you have always wanted to
             watch.</p>
     </div>
 
@@ -28,7 +28,7 @@
                 to reserve your seats anytime from anywhere in the world.</li>
 
             <li class="about-li">Movie Information: Our site offers comprehensive information about the movies you want
-                to watch, including reviews, ratings, and plot summaries, helping you choose the best movie to watch.
+                to watch.
             </li>
         </ul>
     </div>
